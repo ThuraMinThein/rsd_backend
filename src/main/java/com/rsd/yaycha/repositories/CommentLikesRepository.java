c199af0556de0c3edf95881e63e921423c335be2
ThuraMinThein <thur7012@gmail.com>
2025-03-10 15:23:30 +0630

    add total likes in grahpql scheme

--- src/main/java/com/rsd/yaycha/repositories/CommentLikesRepository.java
+++ src/main/java/com/rsd/yaycha/repositories/CommentLikesRepository.java
@@ -3,9 +3,13 @@ package com.rsd.yaycha.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.graphql.data.GraphQlRepository;
 
+import com.rsd.yaycha.entities.Comment;
 import com.rsd.yaycha.entities.CommentLike;
+import com.rsd.yaycha.entities.User;
 
 @GraphQlRepository
 public interface CommentLikesRepository extends JpaRepository<CommentLike, Integer>{
+
+    CommentLike findByCommentAndUser(Comment comment, User user);
 
 }
--- src/main/java/com/rsd/yaycha/repositories/PostLikesRepository.java
+++ src/main/java/com/rsd/yaycha/repositories/PostLikesRepository.java
@@ -3,9 +3,13 @@ package com.rsd.yaycha.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.graphql.data.GraphQlRepository;
 
+import com.rsd.yaycha.entities.Post;
 import com.rsd.yaycha.entities.PostLike;
+import com.rsd.yaycha.entities.User;
 
 @GraphQlRepository
 public interface PostLikesRepository extends JpaRepository<PostLike, Integer> {
+
+    PostLike findByPostAndUser(Post post, User user);
 
 }
